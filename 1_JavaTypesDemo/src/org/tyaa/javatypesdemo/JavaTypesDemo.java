bfbb50f95448f0f4b8377e18d52288f60a547b16
YuriiTrofimenko <tyaa@ukr.net>
2019-10-25 09:27:37 +0300

    init

---
+++ 1_JavaTypesDemo/src/org/tyaa/javatypesdemo/JavaTypesDemo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tyaa.javatypesdemo;
+
+/**
+ *
+ * @author student
+ */
+public class JavaTypesDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // sout + Tab
+        // System.out.println("Hello Java!");
+        // 2^8=256 -> 256/2 = 128 -> -128--1 n 0-127
+        byte x1 = 127;
+        byte x2 = -128;
+        short x3 = 4545;
+        int x = 454543564;
+        long x4 = 454543564346536L;
+        
+        float y1 = 12.55f;
+        double y2 = 12.55d;
+        
+        boolean b1 = true;
+        b1 = false;
+        
+        char c1 = 'A';
+        
+        System.out.println(x);
+    }
+    
+}
