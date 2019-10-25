6db697851e57e661ab7dc10202c397ec570bcd7e
YuriiTrofimenko <tyaa@ukr.net>
2019-10-25 10:27:24 +0300

    ops demo project

---
+++ 2_JavaOpsDemo/src/org/tyaa/javaopsdemo/JavaOpsDemo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tyaa.javaopsdemo;
+
+/**
+ *
+ * @author student
+ */
+public class JavaOpsDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int x = 10;
+        int y = 2;
+        int z = x + y + 12;
+        System.out.println(z);
+        
+        int a;
+        // a = 2 * 2;
+        // a = 20 / 2;
+        // a = 20 - 2;
+        // a = 20 % 2;
+        // a = 21 % 2;
+        // a = 21 % 10;
+        
+        // System.out.println(Math.pow(2, 8));
+        
+        // a = 2 << 8;
+        //System.out.println(a);
+        //System.out.println(a);
+        
+        // a = 21 / 2;
+        
+        // double a1 = 21 / 2;
+        // double a1 = 21.0 / 2.0;
+        // double a1 = 21d / 2d;
+        double a1 = (double)21 / (double)2;
+        System.out.println(a1);
+    }
+    
+}
